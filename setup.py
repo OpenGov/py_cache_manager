f58daa579b7372308a1dfd38b6a9439864ae1f5b
MSeal <mseal007@gmail.com>
2015-02-18 15:39:42 -0800

    Bumped version to fix error in 2.0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def readMD(fname):
     else:
         return read(fname)
 
-version = '2.0.3'
+version = '2.0.4'
 required = [req.strip() for req in read('requirements.txt').splitlines() if req.strip()]
 setup(
     name='CacheMan',
